c2b3df46ccc2e3873b934c848b22fe696fa062b3
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-10-17 17:00:57 +0530

    Show Books added and working.

--- BookStore-fcc/frontend/src/pages/Home.jsx
+++ BookStore-fcc/frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     axios
       .get('http://localhost:5000/books')
       .then((response) => {
-        // console.log(response);
+        console.log(response.data.data);
         setBooks(response.data.data);
         setLoading(false);
       })
@@ -24,22 +24,9 @@ const Home = () => {
         setLoading(false);
       });
   }, []);
+
   return (
     <div className="p-4">
-      <div className="flex justify-center items-center gap-x-4">
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType('table')}
-        >
-          Table
-        </button>
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType('card')}
-        >
-          Card
-        </button>
-      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
@@ -48,10 +35,55 @@ const Home = () => {
       </div>
       {loading ? (
         <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
       ) : (
-        <BooksCard books={books} />
+        <table className="w-full border-seprate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Author
+              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Publish Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => {
+              return (
+                <tr key={book._id} className="h-8">
+                  <td className="border border-slate-700 rounded-md text-center">
+                    {index + 1}
+                  </td>
+                  <td className="border border-slate-700 rounded-md text-center">
+                    {book.title}
+                  </td>
+                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                    {book.author}
+                  </td>
+                  <td className="border border-slate-700 rounded-md text-center">
+                    {book.publishYear}
+                  </td>
+                  <td className="border border-slate-700 rounded-md text-center">
+                    <div className="flex justify-center gap-x-4">
+                      <Link to={`/books/details/${book._id}`}>
+                        <BsInfoCircle className="text-2xl text-green-800" />
+                      </Link>
+                      <Link to={`/books/edit/${book._id}`}>
+                        <AiOutlineEdit className="text-2xl text-yellow-600" />
+                      </Link>
+                      <Link to={`/books/delete/${book._id}`}>
+                        <MdOutlineDelete className="text-2xl text-red-600" />
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       )}
     </div>
   );
