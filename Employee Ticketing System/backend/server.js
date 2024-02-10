d2eec1096054111c3464438f166507b5bf70062c
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-02-10 20:36:21 +0530

    Unable to use .env, added logging facility.

--- Employee Ticketing System/backend/server.js
+++ Employee Ticketing System/backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+const { logger, logEvents } = require('./middleware/logger');
 
-const PORT = 3000;
 const app = express();
+require('dotenv').config();
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+// Will face a CORS error here intially
+const PORT = /*process.env.PORT ||*/ 3000;
+
+/*<------------------------MIDDLEWARE------------------------------------> */
+app.use(logger);
+// ability to process JSON data
+app.use(express.json());
+// ability to render/send static files for anything coming to '/'
+app.use('/', express.static(path.join(__dirname, 'public')));
+// sends these to routes with callbacks
 app.use('/', require('./routes/root'));
+// 404 Page for if something invalid requested
 app.all('*', (req, res) => {
   res.status(404);
   if (req.accepts('html')) {
@@ -19,6 +30,7 @@ app.all('*', (req, res) => {
     res.send('Get off of that microwave dude,atleast use a computer.');
   }
 });
+/*<!-------------------END OF MIDDLEWARE----------------------!> */
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
