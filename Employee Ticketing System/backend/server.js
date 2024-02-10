324be088fc049fe9232a998dc71f471e5d669d0f
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-02-10 13:35:35 +0530

    Landing and 404 pages added.

--- Employee Ticketing System/backend/server.js
+++ Employee Ticketing System/backend/server.js
@@ -1,10 +1,23 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Root Page!');
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', require('./routes/root'));
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts(json)) {
+    res.json({
+      message: '404 Not Found',
+      currentStatus: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    });
+  } else {
+    res.send('Get off of that microwave dude,atleast use a computer.');
+  }
 });
 
 app.listen(PORT, () => {
