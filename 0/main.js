cc4c85aacd54260233e6654f38223d4cb55020f3
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-06-18 22:27:37 +0530

    css and styles added! Favicon worked somehow;(

--- 0/main.js
+++ 0/main.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5500;
 
+function catchError(error) {
+  console.log(error);
+  let errorCode = error.statusCode || 500;
+  res.status(errorCode).send(`An Error occurred due to ${errorCode}.`);
+}
+
+// Now to parse the body sent via POST requests
+app.use(bodyParser.json());
 // Serve static files from the 'static' directory
 app.use(express.static(path.join(__dirname, 'static')));
 // Sever favicon from 'static' directory
@@ -15,9 +24,7 @@ app.get('/', (req, res) => {
   try {
     res.status(200).sendFile(path.join(__dirname, 'static', 'index.html'));
   } catch (error) {
-    console.log(error);
-    let errorCode = error.statusCode || 500;
-    res.status(errorCode).send(`An Error occurred due to ${errorCode}.`);
+    catchError(error);
   }
 });
 
@@ -25,9 +32,17 @@ app.get('/login', (req, res) => {
   try {
     res.status(200).sendFile(path.join(__dirname, 'static', 'login.html'));
   } catch (error) {
-    console.log(error);
-    let errorCode = error.statusCode || 500;
-    res.status(errorCode).send(`An Error occurred due to ${errorCode}.`);
+    catchError(error);
+  }
+});
+
+app.post('/login', (req, res) => {
+  try {
+    const { email, password } = req.body;
+    res.status(200).send(`Welcome ${email}`);
+    console.log(email, password);
+  } catch (error) {
+    catchError(error);
   }
 });
 
@@ -35,9 +50,7 @@ app.get('/signup', (req, res) => {
   try {
     res.status(200).sendFile(path.join(__dirname, 'static', 'signup.html'));
   } catch (error) {
-    console.log(error);
-    let errorCode = error.statusCode || 500;
-    res.status(errorCode).send(`An Error occurred due to ${errorCode}.`);
+    catchError(error);
   }
 });
 
